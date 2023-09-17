c3c5ae3f340d048073294964421bee04036c88cc
turkuazsengul <turkuazsengul@gmail.com>
2023-09-18 01:02:26 +0300

    Initial commit

---
+++ ms-node-configuration/src/main/java/com/example/msnodeconfiguration/MsNodeConfigurationApplication.java
@@ -0,0 +1,15 @@
+package com.example.msnodeconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MsNodeConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNodeConfigurationApplication.class, args);
+	}
+
+}
---
+++ ms-node-configuration/src/test/java/com/example/msnodeconfiguration/MsNodeConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msnodeconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNodeConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
