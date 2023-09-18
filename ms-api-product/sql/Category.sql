f15f8b8d42dc1fba0acea6f5093e54469a4a7738
turkuazsengul <turkuazsengul@gmail.com>
2023-09-18 11:20:54 +0300

    docker-compose.yml updated. sql scripts modified.

--- ms-api-product/sql/Category.sql
+++ ms-api-product/sql/Category.sql
@@ -1,14 +1,12 @@
 ------------ CATEGORY SCRIPTS ------------
 create table category
 (
-    id   serial       not null
-        constraint category_pk
-            primary key,
-    name varchar(255) not null
+    id serial not null constraint category_pk primary key,
+    name varchar(255) not null,
+    sort_order int not null
 );
 
-alter table category
-    owner to postgres;
+alter table category owner to postgres;
 
 INSERT INTO public.category (id, name) VALUES (1, 'Giyim');
 INSERT INTO public.category (id, name) VALUES (2, 'Spor');
@@ -19,16 +17,39 @@ INSERT INTO public.category (id, name) VALUES (4, 'Sanat');
 ------------ SUB-CATEGORY SCRIPTS ------------
 create table sub_category
 (
-    id   serial       not null
-        constraint sub_category_pk
-            primary key,
-    name varchar(255) not null
+    id serial not null constraint sub_category_pk primary key,
+    name varchar(255) not null,
 );
 
-alter table sub_category
-    owner to postgres;
+alter table sub_category owner to postgres;
 
 INSERT INTO public.sub_category (id, name) VALUES (1, 'Futbol');
 INSERT INTO public.sub_category (id, name) VALUES (2, 'Basketbol');
 INSERT INTO public.sub_category (id, name) VALUES (3, 'Tenis');
 INSERT INTO public.sub_category (id, name) VALUES (4, 'Tablo');
+
+------------ CHILD-CATEGORY SCRIPTS ------------
+create table child_category
+(
+    id serial not null constraint child_category_pk primary key,
+    name varchar(255) not null
+);
+
+alter table sub_category owner to postgres;
+
+INSERT INTO public.sub_category (id, name) VALUES (1, 'Futbol');
+INSERT INTO public.sub_category (id, name) VALUES (2, 'Basketbol');
+INSERT INTO public.sub_category (id, name) VALUES (3, 'Tenis');
+INSERT INTO public.sub_category (id, name) VALUES (4, 'Tablo');
+
+------------CATEGORY-TREE SCRIPTS ------------
+create table category_tree
+(
+    id serial not null constraint category_tree_pk primary key,
+    name varchar(255) not null,
+    category_id integer constraint ms_product_category_id_fk references category,
+    sub_category_id integer constraint ms_product_sub_category_id_fk references sub_category,
+    child_category_id integer constraint ms_product_child_category_id_fk references child_category,
+);
+
+alter table sub_category owner to postgres;
--- ms-api-product/sql/Stock.sql
+++ ms-api-product/sql/Stock.sql
@@ -1,14 +1,11 @@
 ------------ STOCK SCRIPTS ------------
 create table stock_status
 (
-    id                serial       not null
-        constraint stock_status_pk
-            primary key,
+    id serial not null constraint stock_status_pk primary key,
     stock_status_name varchar(255) not null
 );
 
-alter table stock_status
-    owner to postgres;
+alter table stock_status owner to postgres;
 
 INSERT INTO public.stock_status (id, stock_status_name) VALUES (4, 'OUTOFSTOCK');
 INSERT INTO public.stock_status (id, stock_status_name) VALUES (2, 'LOWSTOCK');
