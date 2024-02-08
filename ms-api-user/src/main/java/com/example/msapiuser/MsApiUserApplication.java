5620da0884806e2a16f9576fc0c6efe30aa6cb70
turkuazsengul <turkuazsengul@gmail.com>
2024-02-09 00:49:17 +0300

    security configuration and dependencies moved to common-security module

--- ms-api-user/src/main/java/com/example/msapiuser/MsApiUserApplication.java
+++ ms-api-user/src/main/java/com/example/msapiuser/MsApiUserApplication.java
@@ -3,7 +3,6 @@ package com.example.msapiuser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication(scanBasePackages={"com.example.msapiuser","com.common.mscommonsecurity"})
-//@EnableBinding({RegisterServiceMailOperationPublishChannel.class})
 public class MsApiUserApplication {
 
     public static void main(String[] args) {
